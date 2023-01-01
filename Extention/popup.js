1f656848ef808f61b5e774d7e9f23bef15a40956
Syed Ashar <syedashar25@gmail.com>
2023-01-01 14:52:14 -0600

    added comments

--- Extention/popup.js
+++ Extention/popup.js
@@ -1,5 +1,27 @@
 let emailScraper = document.getElementById("scrapeEmail")
+let emailList = document.getElementById("emailList")
 
+// add listener for chrome message (to display the emails on extention)
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let emails = request.emails;
+
+    if (emails == null || emails.length == 0){
+        let li = document.createElement("li");
+        li.innerText = "No emails found";
+        emailList.appendChild(li);
+    }
+    else{
+        emails.forEach((email) => {
+            let li = document.createElement("li");
+            li.innerText = email;
+            emailList.appendChild(li);
+        })
+
+    }
+
+})
+
+// button listener on click
 emailScraper.addEventListener("click", async () => {
 
     // get the active tab
@@ -20,17 +42,18 @@ emailScraper.addEventListener("click", async () => {
 
 })
 
-// function that scrapes
+// function that finds the emails
 function scrapingEmailsOnPage() {
 
     // create regex to recognize email
     const emailsRegex = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
 
-    // match words that appear as the emailsregex
+    // match words that appear as the emails regex
     let emails = document.body.innerHTML.match(emailsRegex);
 
-    // alert
-    alert(emails)
+    //alert(emails);
 
+
+    chrome.runtime.sendMessage({emails});
 
 }
